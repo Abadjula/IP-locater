ac1a897c75206ede0b61d673351484656d47f8f5
Abadjula <76713140+Abadjula@users.noreply.github.com>
2021-03-31 22:48:08 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+while True:
+    import pygeoip
+    Ip_addr = input("Enter the IP address of the user: ")
+    gip = pygeoip.GeoIP("GeoLiteCity.dat")
+    res = gip.record_by_addr(Ip_addr)
+    for key, val in res.items():
+        print('%s : %s' % (key,val))
