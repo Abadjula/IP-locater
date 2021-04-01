c66abce230c10a98e7741926986ba8ce0e25c5cb
Abadjula <76713140+Abadjula@users.noreply.github.com>
2021-03-31 22:55:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 while True:
-    import pygeoip
-    Ip_addr = input("Enter the IP address of the user: ")
-    gip = pygeoip.GeoIP("GeoLiteCity.dat")
-    res = gip.record_by_addr(Ip_addr)
-    for key, val in res.items():
-        print('%s : %s' % (key,val))
+    try:
+        import pygeoip
+        Ip_addr = input("Enter the IP address of the user: ")
+        gip = pygeoip.GeoIP("GeoLiteCity.dat")
+        res = gip.record_by_addr(Ip_addr)
+        for key, val in res.items():
+            print('%s : %s' % (key,val))
+    except:
+        print(f"No IP found {Ip_addr}")
